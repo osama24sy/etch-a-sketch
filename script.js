46b209e8e6748967f99489e1a4dbb325f01864b1
Osama Shbib <osamashbib28@gmail.com>
2022-05-13 23:01:41 +0300

    Add pixel number choosing functionality

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
 const container = document.querySelector(".container");
+const pixelNumInput = document.querySelector("#pixel-num")
+let pixelNum = 16;
 
-for (let i = 0; i < 64; i++) {
+for (let i = 0; i < pixelNum*pixelNum; i++) {
     let minDiv = document.createElement("div");
     minDiv.classList.add("pixel");
     container.appendChild(minDiv);
 }
 
-const pixels = document.querySelectorAll(".pixel")
+pixelNumInput.addEventListener("input", () => {
+    pixelNum = pixelNumInput.value;
+    console.log(pixelNum);
+    container.innerHTML = "";
+    for (let i = 0; i < pixelNum*pixelNum; i++) {
+        let minDiv = document.createElement("div");
+        minDiv.classList.add("pixel");
+        container.appendChild(minDiv);
+    }
+    const pixels = document.querySelectorAll(".pixel");
+    const colorPicker = document.querySelector("#color-picker");
+    let targetColor = "black";
+
+    colorPicker.addEventListener("input", () => {
+        targetColor = colorPicker.value; 
+    }, false);
+
+    pixels.forEach((pixel) => {
+        pixel.addEventListener("mouseover", () => {
+            pixel.style.backgroundColor = targetColor;
+        });
+        pixel.style.width = ((450 / pixelNum) - 2) + "px";
+        pixel.style.height = ((450 / pixelNum) - 2) + "px";
+    });
+});
+
+const pixels = document.querySelectorAll(".pixel");
 const colorPicker = document.querySelector("#color-picker");
 let targetColor = "black";
 
@@ -18,4 +46,6 @@ pixels.forEach((pixel) => {
     pixel.addEventListener("mouseover", () => {
         pixel.style.backgroundColor = targetColor;
     });
+    pixel.style.width = ((450 / pixelNum) - 2) + "px";
+    pixel.style.height = ((450 / pixelNum) - 2) + "px";
 });

ee319271e41a273056f380e98e3a5f6b152fa33a
Osama Shbib <osamashbib28@gmail.com>
2022-05-13 22:25:41 +0300

    Add color changing by hovering functionalities

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ for (let i = 0; i < 64; i++) {
     minDiv.classList.add("pixel");
     container.appendChild(minDiv);
 }
+
+const pixels = document.querySelectorAll(".pixel")
+const colorPicker = document.querySelector("#color-picker");
+let targetColor = "black";
+
+colorPicker.addEventListener("input", () => {
+    targetColor = colorPicker.value; 
+}, false);
+
+pixels.forEach((pixel) => {
+    pixel.addEventListener("mouseover", () => {
+        pixel.style.backgroundColor = targetColor;
+    });
+});
